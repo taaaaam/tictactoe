c10de4bda40fa1cdab595425331b996d154166d2
Tam Vu <tam.vu@yale.edu>
2024-06-18 14:21:45 -0400

    Authentication

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,11 @@ app.use(express.json());
 
 const api_key = 'hnxguf56uj6q';
 const api_secret = 'aprn8x3debn5f3xqpcn76pq9d6s5fdm9efwt739k4jexyjdx5bwxj527bbh5zcga';
-const serverClient = new StreamChat.getInstance(api_key, api_secret);
+const serverClient =  StreamChat.getInstance(api_key, api_secret);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the API");
+});
 
 app.post("/signup", async (req, res) => {
     try {
@@ -24,7 +28,26 @@ app.post("/signup", async (req, res) => {
     }
 })
 
-app.post("/login")
+app.post("/login", async (req, res) => {
+    try {
+    const {username, password} = req.body
+    const {users} = await serverClient.queryUsers({name: username});
+    if (users.length === 0) return res.json({ message: "User not found"});
+    const token = serverClient.createToken(users[0].id);
+    const passwordMatch = await bcrypt.compare(password, users[0].hashedPassword);
+    if (passwordMatch) {
+        res.json({token, 
+            firstName: users[0].firstName,
+            lastName: users[0].lastName,
+            username,
+            userId: users[0].id,
+        
+        });
+    }
+    } catch (error) {
+        res.json(error);
+    }
+});
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
